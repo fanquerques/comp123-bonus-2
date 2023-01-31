fb7b624a368e1c9ec12ca1d9f4e24247af8b28cf
maily <you@example.com>
2023-01-31 09:00:28 -0500

    Add project files.

---
+++ Plane.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp123_bonus2_fan_yang
+{
+    // Fan Yang (301295721)
+    internal class Plane
+    {
+        public string Manufactory 
+        {
+            get;
+            set;
+        }
+        public string Model 
+        {
+            get;
+            set;
+        }
+        public int Capacity
+        {
+            get;
+            set;
+        }
+        public string Type
+        {
+            get;
+            set;
+        }
+        public string SerialNumber
+        {
+            get;
+            set;
+        }
+
+        public Plane(){
+            this.Manufactory = "unknown";
+            this.Model = "unknown";
+            this.Capacity = 0;
+            this.Type = "unassigned";
+            this.SerialNumber = "00000";   
+        }
+
+        public Plane(string manufactory, string model, int capacity, string type, string serialNumber)
+        {
+            Manufactory = manufactory;
+            Model = model;
+            Capacity = capacity;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+
+        public bool HasCapacity(int numOfPeople){
+
+            return numOfPeople <= Capacity;
+        }
+
+        public override string ToString()
+        {
+            return $"Manufactory: {Manufactory}, Model: {Model}," +
+                $" Capacity: {Capacity}, Type: {Type}, Serial Number: {SerialNumber}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace comp123_bonus2_fan_yang
+{
+    internal class Program
+    {
+        // Fan Yang (301295721)
+        static void Main(string[] args)
+        {
+            List<Plane> listOfPlane = new List<Plane>();
+            Plane plane1 = new Plane();
+            Plane plane2 = new Plane("Tesla", "T", 4, "passenger", "12345");
+            Plane plane3 = new Plane();
+            Plane plane4 = new Plane("Tesla", "X", 6, "military", "52521");
+            Plane plane5 = new Plane("Kia", "L", 2, "passenger", "00010");
+
+            listOfPlane.Add(plane1);
+            listOfPlane.Add(plane2);
+            listOfPlane.Add(plane3);
+            listOfPlane.Add(plane4);
+            listOfPlane.Add(plane5);
+
+            foreach (Plane plane in listOfPlane)
+            {
+                Console.WriteLine(plane.ToString());
+            }
+
+            Console.WriteLine("\n" + "Plane 1 has capacity of 0: " + plane1.HasCapacity(0));
+            Console.WriteLine("Plane 1 has capacity of 1: " + plane1.HasCapacity(1));
+            Console.WriteLine("Plane 2 has capacity of 4: " + plane2.HasCapacity(4));
+            Console.WriteLine("Plane 2 has capacity of 5: " + plane2.HasCapacity(5));
+            Console.WriteLine("Plane 4 has capacity of 0: " + plane4.HasCapacity(0));
+            Console.WriteLine("Plane 4 has capacity of 6: " + plane4.HasCapacity(6));
+            Console.WriteLine("Plane 4 has capacity of 7: " + plane4.HasCapacity(7));
+
+        }
+    }
+}
